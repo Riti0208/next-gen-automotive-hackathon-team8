5e1355e7e02dd03df304d223bcd417af1f99b148
yoshieng <yoshidetl.eng@gmail.com>
2026-01-17 16:03:48 +0900

    Add Supporter's Page

---
+++ src/app/supporter/page.tsx
@@ -0,0 +1,9 @@
+import { SupporterMap } from "@/components/supporter-map";
+
+export default function SupporterPage() {
+  return (
+    <main className="flex h-screen flex-col">
+      <SupporterMap />
+    </main>
+  );
+}
